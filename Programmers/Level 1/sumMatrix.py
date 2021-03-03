e5e68dd1b74b1b39d9be0dff232e327d5529a587
wonho <59119468+wonho1401@users.noreply.github.com>
2021-03-03 10:44:45 +0900

    sum of matrix

--- Programmers/Level 1/sumMatrix.py
+++ Programmers/Level 1/sumMatrix.py
@@ -1,9 +1,10 @@
 def solution(arr1, arr2):
-    answer = []
-    o = []
-    for (a, b) in zip(arr1, arr2):
-        for (c, d) in zip(a, b):
-            o.append(c + d)
-        answer.append(o)
-        o = []
-    return answer
+    # t = []
+    # answer = []
+    # for i,j in zip(arr1,arr2):
+    #     for x,y in zip(i,j):
+    #         t.append(x+y)
+    #     answer.append(t)
+    #     t=[]
+    # return answer
+    return [[x+y for x,y in zip(i,j) ] for i,j in zip(arr1,arr2)]
